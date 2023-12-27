11c5759987c8475ce285c0d31ded8f5a100d9db5
taher mohamed <107802873+taher77777@users.noreply.github.com>
2023-12-27 21:23:25 +0200

    Add files via upload

---
+++ TIMER0_interface.h
@@ -0,0 +1,16 @@
+/********************		Author: TAHER MOHAMED	**************/
+/********************		Layer: MCAL				**************/
+/********************		SWC: TIMER				**************/
+/********************		Version: 1.00			**************/
+/********************		Date: 28-8-2020			**************/
+/*****************************************************************/
+/*****************************************************************/
+
+#ifndef TIMER_INTERFACE_H_
+#define TIMER_INTERFACE_H_
+
+void TIMER0_voidInit(void);
+u8 TIMER0_u8SetCallBack(void (*copy_pvCallBackFunc)(void));
+void TIMER0_voidSetPreLoadValue (u32 TIMER0_u32PreValue);
+void TIMER0_voidSeOCR0Value(u32 TIMER0_u32OCR0Value);
+#endif
